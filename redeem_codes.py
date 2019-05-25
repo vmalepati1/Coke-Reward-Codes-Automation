d71f1036fad0034e46a77072e808d55c10117570
vmalepati1 <malepativikas@gmail.com>
2019-05-25 18:43:12 -0400

    Added log files that show the status of code redemption. -VM
    Added log files that show the status of code redemption. -VM

--- redeem_codes.py
+++ redeem_codes.py
@@ -4,11 +4,12 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.common.keys import Keys
-import pandas as pd
 import sys
+import csv
+import time
 
-if len(sys.argv) != 6:
-    print('Usage: python redeem_codes.py twitter_user_name twitter_password desired_rewards reward_codes_file_path element_load_timeout')
+if len(sys.argv) != 7:
+    print('Usage: python redeem_codes.py twitter_user_name twitter_password desired_rewards reward_codes_file_path log_file_path element_load_timeout')
     sys.exit(1)
 
 # Configurable parameters
@@ -17,8 +18,9 @@ twitter_password = sys.argv[2]
 # Which awards you would like to distribute your codes to (see the below rewards table for all available rewards)
 desired_rewards = sys.argv[3].split(',')
 reward_codes_file_path = sys.argv[4]
+log_file_path = sys.argv[5]
 # How long to wait for UI elements to load
-element_load_timeout = int(sys.argv[5])
+element_load_timeout = int(sys.argv[6])
 
 driver = webdriver.Chrome('chromedriver_win32/chromedriver.exe')
 
@@ -30,6 +32,8 @@ def wait_for_element_to_load(xpath : str):
 
 # Sign in using twitter account (you can change the sign in method by changing the clicks to elements specified below)
 login_icon = wait_for_element_to_load('//*[@id="icon_login"]')
+# Wait extra (login button takes long time to load for some reason)
+time.sleep(5)
 login_icon.click()
 login_button = wait_for_element_to_load('//*[@id="nav-profile-dropdown"]/ul[1]/li[1]')
 login_button.click()
@@ -60,8 +64,8 @@ rewards_table = [
     ('Magazines', '//*[@id="enter_code_id_content_coke2016_en_offers_jcr_content_pageContent_responsivecolumncont_1949744869_column0_codeentry_a45c"]'),
     ('iTunes', '//*[@id="enter_code_id_content_coke2016_en_offers_jcr_content_pageContent_responsivecolumncont_1616209238_column0_codeentry_1135"]'),
     ('Nordstrom', '//*[@id="enter_code_id_content_coke2016_en_offers_jcr_content_pageContent_responsivecolumncont_1356747955_column0_codeentry_e30c"]'),
-    ('Chilis', '//*[@id="enter_code_id_content_coke2016_en_offers_jcr_content_pageContent_codeentry_d92e"]'),
-    ('Shutterfly', '//*[@id="enter_code_id_content_coke2016_en_offers_jcr_content_pageContent_responsivecolumncont_1164568584_column0_codeentry_9cf"]'),
+    #('Chilis', '//*[@id="enter_code_id_content_coke2016_en_offers_jcr_content_pageContent_codeentry_d92e"]'),
+    #('Shutterfly', '//*[@id="enter_code_id_content_coke2016_en_offers_jcr_content_pageContent_responsivecolumncont_1164568584_column0_codeentry_9cf"]'),
     ('Drink', '//*[@id="enter_code_id_content_coke2016_en_offers_jcr_content_pageContent_responsivecolumncont_285521029_column0_codeentry_ec58"]'),
     ('Groceries', '//*[@id="enter_code_id_content_coke2016_en_offers_jcr_content_pageContent_responsivecolumncont_140468331_column0_codeentry_6b0d"]'),
     ('Coffee', '//*[@id="enter_code_id_content_coke2016_en_offers_jcr_content_pageContent_responsivecolumncont_353597540_column0_codeentry_49d0"]'),
@@ -69,16 +73,46 @@ rewards_table = [
     ('UHDTV', '//*[@id="enter_code_id_content_coke2016_en_offers_jcr_content_pageContent_responsivecolumncont_1067140601_column0_codeentry_a9fd"]')
 ]
 
-data = pd.read_csv(reward_codes_file_path)
+with open(reward_codes_file_path) as csv_file:
+    with open(log_file_path, mode='a') as log_file:
+        number_of_codes_tested = 0
 
-number_of_codes_tested = 0
-for code in data['codes']:
-    reward_index = number_of_codes_tested % len(desired_rewards)
-    desired_reward_name = desired_rewards[reward_index]
-    desired_reward_xpath = [item[1] for item in rewards_table if item[0] == desired_reward_name][0]
-    desired_reward_input = wait_for_element_to_load(desired_reward_xpath)
-    ActionChains(driver).move_to_element(desired_reward_input).perform()
-    desired_reward_input.clear()
-    desired_reward_input.send_keys(code)
-    desired_reward_input.submit()
-    number_of_codes_tested += 1
+        csv_reader = csv.reader(csv_file)
+        log_writer = csv.writer(log_file, lineterminator = '\n')
+
+        # Skip header row
+        next(csv_reader)
+
+        # Write log header
+        log_writer.writerow(['codes', 'status'])
+        
+        for row in csv_reader:
+            # Reward code should be stored in first column
+            code = row[0]
+            reward_index = number_of_codes_tested % len(desired_rewards)
+            desired_reward_name = desired_rewards[reward_index]
+            desired_reward_xpath = [item[1] for item in rewards_table if item[0] == desired_reward_name][0]
+            # Error message text element xpath derived from the input box xpath
+            error_text_container_xpath = desired_reward_xpath.replace('enter_code_', '')
+            error_text_xpath = error_text_container_xpath + '/section[1]/form/div[3]/div[1]/ng-bind-html/div/div/p'
+            desired_reward_input = wait_for_element_to_load(desired_reward_xpath)
+            ActionChains(driver).move_to_element(desired_reward_input).perform()
+            desired_reward_input.clear()
+            desired_reward_input.send_keys(code)
+            desired_reward_input.submit()
+            # Brief pause between each entry to allow the page to process previous input
+            time.sleep(5)
+            
+            try:          
+                error_text = driver.find_element_by_xpath(error_text_xpath).text
+
+                if error_text == 'Please enter a valid code.':
+                    log_writer.writerow([code, 'invalid'])
+                elif error_text == 'OOPS! This code has already been redeemed.':
+                    log_writer.writerow([code, 'already_redeemed'])
+                else:
+                    # Weird case scenario
+                    log_writer.writerow([code, error_text])
+            except:
+                log_writer.writerow([code, 'success'])
+            number_of_codes_tested += 1
